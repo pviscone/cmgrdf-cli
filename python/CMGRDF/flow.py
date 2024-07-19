bd1e2dd4e0976b03b8f93b703d6d38c90fda3b2d
Piero Viscone <piero.viscone@cern.ch>
2024-07-19 23:39:42 +0200

    Added process options in the flowsteps to run them just on processes defined by regex

--- python/CMGRDF/flow.py
+++ python/CMGRDF/flow.py
@@ -1,4 +1,5 @@
 import copy
+import re
 import ROOT
 from CMGRDF.data import Sample
 from CMGRDF.utils import _recursiveAddToHash, safeName
@@ -28,18 +29,24 @@ class FlowStep(object):
 
     """
 
-    def __init__(self, name, onMC=True, onDataDriven=True, onData=True, eras=None):
+    def __init__(self, name, onMC=True, onDataDriven=True, onData=True, eras=None, process=None):
+        if process and not onMC:
+            raise RuntimeError("Cannot specify process pattern for a non-MC step")
+
         self.name = name
         self.onMC = onMC
         self.onData = onData
         self.onDataDriven = onDataDriven
         self.eras = eras
+        self.process = process #regex pattern
 
     def appliesTo(self, sample : Sample, era) -> bool:
         assert isinstance(sample, Sample)
         if sample.isMC:
             if not self.onMC:
                 return False
+            elif self.process:
+                return bool(re.match(self.process,sample.process))
         elif sample.isData:
             if not self.onData:
                 return False
