3ebc104f0f9dec8526b489c9ac44e5e6905669bb
Sergio Sanchez Cruz <sergio.sanchez.cruz@cern.ch>
2024-07-25 09:34:32 +0200

    changing cms eras naming

--- python/CMGRDF/cms/MuonSFs.py
+++ python/CMGRDF/cms/MuonSFs.py
@@ -6,7 +6,7 @@ from CMGRDF.CorrectionlibFactory import CorrectionlibFactory
 from CMGRDF.cms.eras import run2eras
 
 muonSFPath = os.environ["MUON_SF_PATH"] if "MUON_SF_PATH" in os.environ else "/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/MUO"
-muonPOGEras = {"2016pre": "2016preVFP_UL", "2016post": "2016postVFP_UL", "2017": "2017_UL", "2018": "2018_UL"}
+muonPOGEras = {"2016APV": "2016preVFP_UL", "2016": "2016postVFP_UL", "2017": "2017_UL", "2018": "2018_UL"}
 
 
 class MuonIDSFDefine(Define):
