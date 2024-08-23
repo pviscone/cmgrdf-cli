0a61148a5656414090adeec52f331268cd9ecd95
Piero Viscone <piero.viscone@cern.ch>
2024-08-23 14:18:03 +0200

    allow sample argument on all the samples

--- python/CMGRDF/flow.py
+++ python/CMGRDF/flow.py
@@ -30,9 +30,6 @@ class FlowStep(object):
     """
 
     def __init__(self, name, onMC=True, onDataDriven=True, onData=True, eras=None, sample=None, **options):
-        if sample and not onMC:
-            raise RuntimeError("Cannot specify sample pattern for a non-MC step")
-
         self.name = name
         self.onMC = onMC
         self.onData = onData
