ae0e65a813756adf6aa3eb17bd6ea25fe840bd51
Piero Viscone <piero.viscone@cern.ch>
2024-10-31 16:24:36 +0100

    export range flowstep in main cmgrdf init

--- python/CMGRDF/__init__.py
+++ python/CMGRDF/__init__.py
@@ -11,7 +11,7 @@ ROOT.gInterpreter.ProcessLine('#include "jsonFilter.h"')
 
 from CMGRDF.utils import MultiKey, MultiReport, localOrEOS, NormUncertainty
 from CMGRDF.data import Source, MCSample, MCGroup, DataDrivenSample, DataSample, Process, Data
-from CMGRDF.flow import Define, ReDefine, DefineDefault, Alias, Vary, Cut, AddWeight, AddWeightUncertainty, Marker, Flow, Yield
+from CMGRDF.flow import Define, ReDefine, DefineDefault, Alias, Vary, Cut, AddWeight, AddWeightUncertainty, Marker, Flow, Yield, Range
 from CMGRDF.snapshot import Snapshot
 from CMGRDF.plots import Plot, PlotResult, PlotSetPrinter
 from CMGRDF.processor import Processor
