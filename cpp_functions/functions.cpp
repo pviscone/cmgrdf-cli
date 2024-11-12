b6b7ce9e36b787aa1d6647bd4939d9f741c4ebcc
Piero Viscone <piero.viscone@cern.ch>
2024-11-12 20:31:36 +0100

    made Electron smearing much more efficient

--- cpp_functions/functions.cpp
+++ cpp_functions/functions.cpp
@@ -6,9 +6,10 @@ using namespace ROOT::VecOps;
 using namespace ROOT::Math;
 using namespace ROOT;
 
+auto generator = TRandom();
+
 //_____________________________________________________________________________
 
 float if3(bool cond, float iftrue, float iffalse) {
     return cond ? iftrue : iffalse;
 }
-
--- flows/SFs/electronSS.py
+++ flows/SFs/electronSS.py
@@ -21,44 +21,55 @@ class electronSmearing(BaseCorrection):
     def init(self, era=None):
         if era is None:
             raise ValueError("Must be defined with one and only one era")
-        self.era = era
-        self.eras = [era]
 
-        self.corrector = CorrectionlibFactory.loadCorrector(corrections_map[self.era], "Smearing", check=True)[0]
+        self.corrector = CorrectionlibFactory.loadCorrector(corrections_map[era], "Smearing", check=True)[0]
+
         cpp_sf = """
-            ROOT::RVec<float> electronSmearingSF_<era> (const std::string &syst_type, const ROOT::RVec<float> &pt, const ROOT::RVec<float> &eta, const ROOT::RVec<float> &r9){
+            ROOT::RVec<float> electronSmearingSF_<era> (ROOT::RVec<float> &pt, const ROOT::RVec<float> &eta, const ROOT::RVec<float> &r9){
                 int n = eta.size();
-                auto generator = TRandom();
-                ROOT::RVec<float> sf(n);
                 for (int i = 0; i < n; i++) {
                     float rho = <corrector>->evaluate({"rho", eta[i], r9[i]});
-                    if (syst_type == "up"){
-                        rho += <corrector>->evaluate({"err_rho", eta[i], r9[i]});
-                    } else if (syst_type == "down"){
-                        rho -= <corrector>->evaluate({"err_rho", eta[i], r9[i]});
-                    } else if (syst_type != "nominal"){
-                        throw std::invalid_argument("Unknown syst_type: " + syst_type);
-                    }
-                    sf[i] = generator.Gaus(1., rho);
+                    pt[i] *= generator.Gaus(1., rho);
                 }
-                return sf*pt;
+                return pt;
             }
-        """.replace("<era>", self.era).replace("<corrector>", self.corrector)
+        """.replace("<era>", era).replace("<corrector>", self.corrector)
+
+        cpp_up = """
+            ROOT::RVec<float> electronSmearing_up_<era> (ROOT::RVec<float> &pt, const ROOT::RVec<float> &eta, const ROOT::RVec<float> &r9){
+                int n = eta.size();
+                for (int i = 0; i < n; i++) {
+                    float rho = <corrector>->evaluate({"rho", eta[i], r9[i]}) + <corrector>->evaluate({"err_rho", eta[i], r9[i]});
+                    pt[i] *= generator.Gaus(1., rho);
+                }
+                return pt;
+            }
+        """.replace("<era>", era).replace("<corrector>", self.corrector)
+
+        cpp_down = """
+            ROOT::RVec<float> electronSmearing_down_<era> (ROOT::RVec<float> &pt, const ROOT::RVec<float> &eta, const ROOT::RVec<float> &r9){
+                int n = eta.size();
+                for (int i = 0; i < n; i++) {
+                    float rho = <corrector>->evaluate({"rho", eta[i], r9[i]}) - <corrector>->evaluate({"err_rho", eta[i], r9[i]});
+                    pt[i] *= generator.Gaus(1., rho);
+                }
+                return pt;
+            }
+        """.replace("<era>", era).replace("<corrector>", self.corrector)
 
         ROOT.gInterpreter.Declare(cpp_sf)
+        ROOT.gInterpreter.Declare(cpp_up)
+        ROOT.gInterpreter.Declare(cpp_down)
 
         branch_name = self.pt if not bool(self.defineNew) else self.defineNew
         return BranchCorrection(
             branch_name,
-            f'electronSmearingSF_{self.era}("nominal", {self.pt}, {self.eta}, {self.r9})',
-            f'electronSmearingSF_{self.era}("up", _OLD_{self.pt}_, {self.eta}, {self.r9})',
-            f'electronSmearingSF_{self.era}("down", _OLD_{self.pt}_, {self.eta}, {self.r9})',
+            f'electronSmearingSF_{era}({self.pt}, {self.eta}, {self.r9})',
+            f'electronSmearing_up_{era}(_OLD_{self.pt}_, {self.eta}, {self.r9})',
+            f'electronSmearing_down_{era}(_OLD_{self.pt}_, {self.eta}, {self.r9})',
             preserve=[self.pt],
             onData=False,
             onDataDriven=False,
-            era=self.era,
-            doSyst=self.doSyst,
-            nuisName=self.nuisName,
             redefine=not bool(self.defineNew),
         )
 
