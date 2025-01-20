60f3af9c734eac56a6c6b277253c61119300ff39
Piero Viscone <piero.viscone@cern.ch>
2025-01-20 19:20:46 +0100

    fixed efficiency

--- cpp_functions/matching.cpp
+++ cpp_functions/matching.cpp
@@ -26,27 +26,3 @@ RVec<bool> match_mask(
     return mask;
 }
 
-RVec<bool> match_mask(
-        const RVecF &obj1_pt,
-        const RVecF &obj1_eta,
-        const RVecF &obj1_phi,
-        const float &obj2_pt,
-        const float &obj2_eta,
-        const float &obj2_phi,
-        float dRcut = 0.1){
-
-    RVec<bool> mask(obj1_eta.size(), false);
-    RVecF dR=DeltaR(obj1_eta, RVecF(obj1_eta.size(),obj2_eta), obj1_phi, RVecF(obj1_eta.size(),obj2_phi));
-    float minabs_pt=FLT_MAX;
-    int minabs_pt_index=-1;
-    for (int i = 0; i < obj1_eta.size(); i++) {
-        if (dR[i] < dRcut && abs(obj1_pt[i]-obj2_pt)<minabs_pt) {
-            minabs_pt=abs(obj1_pt[i]-obj2_pt);
-            minabs_pt_index=i;
-        }
-    }
-    if (minabs_pt_index!=-1) {
-        mask[minabs_pt_index] = true;
-    }
-    return mask;
-}
--- flows/efficiency.py
+++ flows/efficiency.py
@@ -4,24 +4,36 @@ from flows.SFs.electronID import electronID
 
 from CMGRDF.collectionUtils import DefineSkimmedCollection
 
-flow = Flow(
-    "efficiency",
-    [
-    #! ---------------------- Gen definition ---------------------- #
-    DefineSkimmedCollection("GenZd", "GenPart", mask="GenPart_pdgId==32 && (GenPart_statusFlags & (1<<13)) "),
-    DefineSkimmedCollection("GenEle", "GenPart", mask="abs(GenPart_pdgId)==11 && (GenPart_statusFlags & 1<<13) && (GenPart_statusFlags & 1<<8)"),
 
-    #! ------------------------ SanityChecks ----------------------- #
-    Cut("1GenZd", "nGenZd==1"),
-    Cut("2GenEle", "nGenEle==2", plot="NoCut"),
+def flow(pair="PFPF"):
+    #!TODO FIx PFlowPt: Electron collection have PF-lowPt duplicates
+    if pair == "PFPF":
+        cutpair = DefineSkimmedCollection(f"{pair}MatchedDiEle", "MatchedDiEle", mask = "Take(Electron_isPF, MatchedDiEle_l1idx) && Take(Electron_isPF, MatchedDiEle_l2idx)")
+    elif pair == "lowPtlowPt":
+        cutpair = DefineSkimmedCollection(f"{pair}MatchedDiEle", "MatchedDiEle", mask = "Take(Electron_isLowPt, MatchedDiEle_l1idx) && Take(Electron_isLowPt, MatchedDiEle_l2idx)")
+    elif pair == "PFlowPt":
+        cutpair = DefineSkimmedCollection(f"{pair}MatchedDiEle", "MatchedDiEle", mask = "(Take(Electron_isLowPt, MatchedDiEle_l1idx) && Take(Electron_isPF, MatchedDiEle_l2idx)) || (Take(Electron_isPF, MatchedDiEle_l1idx) && Take(Electron_isLowPt, MatchedDiEle_l2idx))")
 
-    #! -------------------------- Matching ------------------------- #
-    Cut("nDiEle", "nDiElectron>0"),
-    DefineSkimmedCollection("MatchedDiEle", "DiElectron", mask="match_mask(DiElectron_pt, DiElectron_eta, DiElectron_phi, GenZd_pt[0], GenZd_eta[0], GenZd_phi[0])"),
-    Cut("1MatchedDiEle", "nMatchedDiEle==1", plot="1MatchedDiEle"),
 
-    #! --------------------------- PFPF ---------------------------- #
-    DefineSkimmedCollection("PFPFMatchedDiEle", "MatchedDiEle", mask = "Take(Electron_isPF, MatchedDiEle_l1idx) && Take(Electron_isPF, MatchedDiEle_l2idx)"),
-    Cut("1DiElePFPF", "nPFPFMatchedDiEle>0", plot="1PFPFMatchedDiEle"),
-    ],
-)
+
+    return Flow(
+        "efficiency",
+        [
+        #! ---------------------- Gen definition ---------------------- #
+        DefineSkimmedCollection("GenZd", "GenPart", mask="GenPart_pdgId==32 && (GenPart_statusFlags & (1<<13)) "),
+        DefineSkimmedCollection("GenEle", "GenPart", mask="abs(GenPart_pdgId)==11 && (GenPart_statusFlags & 1<<13) && (GenPart_statusFlags & 1<<8)"),
+
+        #! ------------------------ SanityChecks ----------------------- #
+        Cut("1GenZd", "nGenZd==1"),
+        Cut("2GenEle", "nGenEle==2", plot="NoCut"),
+
+        #! -------------------------- Matching ------------------------- #
+        Cut("nDiEle", "nDiElectron>0"),
+        DefineSkimmedCollection("MatchedDiEle", "DiElectron", mask="match_mask(DiElectron_eta, DiElectron_phi, GenZd_eta[0], GenZd_phi[0])"),
+        Cut("1MatchedDiEle", "nMatchedDiEle>0", plot="1MatchedDiEle"),
+
+        #! --------------------------- pair type cut ---------------------------- #
+        cutpair,
+        Cut(f"1DiEle{pair}", f"n{pair}MatchedDiEle>0", plot=f"1{pair}MatchedDiEle"),
+        ],
+    )
