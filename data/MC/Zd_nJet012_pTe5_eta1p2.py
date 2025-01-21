295bab9dd7afa3e1611a6ea8c39ced20d59dbe70
Piero Viscone <piero.viscone@cern.ch>
2025-01-21 18:22:00 +0100

    defined M3p1 and M1 signal samples

--- data/MC/Zd_nJet012_pTe5_eta1p2.py
+++ data/MC/Zd_nJet012_pTe5_eta1p2.py
@@ -19,6 +19,44 @@ MCDict={
         ],
         "label":"ZdM5",
         "color": ROOT.kBlue-3,
+    },
+    "HAHM_13p6TeV_M3p1" : {
+        "groups" : [
+            {
+            "name": "HAHM_13p6TeV_M3p1",
+            "samples": {
+                "HAHM_13p6TeV_M3p1":
+                    {
+                    "xsec":None,
+                    "path": "signalSamples/HAHM_DarkPhoton_13p6TeV_Nov2024/NANOAOD_noskim_allpairs/CRAB_UserFiles/crab_{name}/250107_182017/0000/DoubleElectronNANO_Run3_{era}_mc_2025Jan07_1.root"
+                    },
+                },
+            "cut":"1",
+            "genWeightName": None,
+            "weight": "1",
+            },
+        ],
+        "label":"ZdM5",
+        "color": ROOT.kRed,
+    },
+    "HAHM_13p6TeV_M1" : {
+        "groups" : [
+            {
+            "name": "HAHM_13p6TeV_M1",
+            "samples": {
+                "HAHM_13p6TeV_M1":
+                    {
+                    "xsec":None,
+                    "path": "signalSamples/HAHM_DarkPhoton_13p6TeV_Nov2024/NANOAOD_noskim_allpairs/CRAB_UserFiles/crab_{name}/250107_182031/0000/DoubleElectronNANO_Run3_{era}_mc_2025Jan07_1.root"
+                    },
+                },
+            "cut":"1",
+            "genWeightName": None,
+            "weight": "1",
+            },
+        ],
+        "label":"ZdM5",
+        "color": ROOT.kGreen,
     }
 }
 
