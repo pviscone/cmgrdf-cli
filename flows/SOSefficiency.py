e53d390906b24fb366994936d07c75c8e9003e09
Piero Viscone <piero.viscone@cern.ch>
2025-01-25 00:32:01 +0100

    implementet SOS efficiency (not working)

--- flows/SOSefficiency.py
+++ flows/SOSefficiency.py
@@ -36,17 +36,24 @@ def flow(pair="PFPF"):
         Define("GenZd_invPt","(GenEle_p4[0]+GenEle_p4[1]).pt()"),
         Cut("noCut", "1",  plot="NoCut"),
 
+        #! ----------------------- Overlap cleaning ---------------------#
+        Define("_DiElectronOverlapMask","!Take(Electron_isPFoverlap,DiElectron_l1idx) && !Take(Electron_isPFoverlap,DiElectron_l2idx)"),
+        DefineSkimmedCollection("DiElectron", "DiElectron", mask="_DiElectronOverlapMask", redefine = True),
+
         #! -------------------------- Matching ------------------------- #
         Cut("nDiEle", "nDiElectron>0"),
-        Define("Electron_matchMask1", "match_mask(Electron_eta, Electron_phi, GenEle_eta[0], GenEle_phi[0])"),
-        Define("Electron_matchMask2", "match_mask(Electron_eta, Electron_phi, GenEle_eta[1], GenEle_phi[1])"),
-        DefineSkimmedCollection("MatchedDiEle", "DiElectron",
-            mask="""
-            !Take(Electron_isPFoverlap,DiElectron_l1idx) && !Take(Electron_isPFoverlap,DiElectron_l2idx) &&
-            (
-                (Take(Electron_matchMask1,DiElectron_l1idx) && Take(Electron_matchMask2,DiElectron_l2idx)) ||
-                (Take(Electron_matchMask2,DiElectron_l1idx) && Take(Electron_matchMask1,DiElectron_l2idx))
-            )"""),
+
+        Define("Electron_dRgen1", "dR(Electron_eta, GenEle_eta[0], Electron_phi, GenEle_phi[0])"),
+        Define("Electron_dRgen2", "dR(Electron_eta, GenEle_eta[1], Electron_phi, GenEle_phi[1])"),
+        Define("Electron_dPtgen1", "abs(Electron_pt-GenEle_pt[0])/Electron_pt"),
+        Define("Electron_dPtgen2", "abs(Electron_pt-GenEle_pt[1])/Electron_pt"),
+        Define("Electron_dVtxgen1", "deltaVtx(Electron_vx, Electron_vy, Electron_vz, GenEle_vx[0], GenEle_vy[0], GenEle_vz[0])"),
+        Define("Electron_dVtxgen2", "deltaVtx(Electron_vx, Electron_vy, Electron_vz, GenEle_vx[1], GenEle_vy[1], GenEle_vz[1])"),
+        Define("DiElectron_dVtx", "deltaVtx(DiElectron_sv_x, DiElectron_sv_y, DiElectron_sv_z, GenEle_vx[0], GenEle_vy[0], GenEle_vz[0])"),
+        Define("DiElectron_matchInfo", "SOS_matching(DiElectron_l1idx, DiElectron_l2idx, DiElectron_dVtx, Electron_dRgen1, Electron_dRgen2, Electron_dPtgen1, Electron_dPtgen2)"),
+
+        #! MATCHING AFTER OVERLAP REMOVAL
+        DefineSkimmedCollection("MatchedDiEle", "DiElectron", mask="DiElectron_matchInfo.match"),
         Cut("1MatchedDiEle", "nMatchedDiEle>0", plot="GenMatching"),
 
         #! --------------------------- pair type cut ---------------------------- #
