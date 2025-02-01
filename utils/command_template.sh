c66610297efaccb9c6f8f9d3b5a2828953a22086
Piero Viscone <piero.viscone@cern.ch>
2025-02-01 03:39:59 +0100

    command look for cmgrdf, if can't be found, clone it

--- utils/command_template.sh
+++ utils/command_template.sh
@@ -1,10 +1,34 @@
-#This block of code just warn you if you are using a different version of CMGRDF than the one you used to run tha analysis
-pwd=$(pwd)
+#!/bin/bash
+
+zlog="$(dirname "$(realpath "$0")")"
+
+if [[ -z "$CMGRDF" ]] && [ ! -d $zlog/cmgrdf-prototype ]; then
+    echo "CMGRDF is not set, I am cloning it for you, lazy human."
+    read -p "Are you sure (it's a big repo to clone again) (y/n)?" CONT
+    if [ "$CONT" = "y" ]; then
+        git clone --recursive https://gitlab.cern.ch/cms-new-cmgtools/cmgrdf-prototype.git
+        cd $zlog/cmgrdf-prototype
+        if [ -f cmgrdf_commit.txt ]; then
+            read -r saved_commit < cmgrdf_commit.txt
+            git checkout --recurse-submodules $saved_commit
+        else
+            echo "CMGRDF commit hash not found, cloning last commit of dpee branch"
+            git checkout --recurse-submodules dpee
+        fi
+        cd $zlog
+        echo "CMGRDF cloned"
+        echo "Starting to build CMGRDF"
+        source $zlog/setup.sh build
+    else
+        exit 1
+    fi
+fi
+
 cd $CMGRDF
 commit_hash=`git describe --match=NeVeRmAtCh --always --abbrev=40 --dirty`
 current_diff=$(git diff)
-cd $pwd
-
+cd $zlog
+#This block of code just warn you if you are using a different version of CMGRDF than the one you used to run tha analysis
 if [ -f cmgrdf_commit.txt ]; then
     read -r saved_commit < cmgrdf_commit.txt
     if [ "$commit_hash" = "$saved_commit" ]; then
