cf7131853d9da083e13b6f7b96cb13504bab320d
Piero Viscone <piero.viscone@cern.ch>
2025-02-02 23:39:20 +0100

    fixed plot defaults

--- plots/defaults.py
+++ plots/defaults.py
@@ -13,21 +13,21 @@ histo2d_defaults = OrderedDict()
 # The first matched pattern will be used
 # TODO xTitle like a regex pattern? re.replace(pattern, replacement, string)
 name_defaults = OrderedDict({
-    "(.*)_(:?pt|invPt)(.*)": dict(
+    "(.*)_(?:pt|invPt)(.*)": dict(
         bins=(50, 0, 50),
-        label="($1) ($2) p_{T} [GeV]",
+        label="($1) ($2) $p_{T}$ [GeV]",
     ),
     "(.*)_eta(.*)": dict(
         bins=(30, -2, 2),
-        label="($1) ($2) \eta",
+        label="($1) ($2) $\eta$",
     ),
     "(.*)_abseta(.*)": dict(
         bins=(30, 0, 2),
-        label="($1) ($2) |\eta|",
+        label="($1) ($2) $|\eta|$",
     ),
     "(.*)_phi(.*)": dict(
         bins=(30, -3.14, 3.14),
-        label="($1) ($2) \phi",
+        label="($1) ($2) $\phi$",
     ),
     "(.*)_(mass|invMass)(.*)": dict(
         bins=(40, 0, 6),
@@ -35,7 +35,7 @@ name_defaults = OrderedDict({
     ),
     "(.*)_dR(.*)": dict(
         bins=(40, 0, 1.5),
-        label="($1) ($2) \Delta R",
+        label="($1) ($2) $\Delta$R",
     ),
     "(.*)_type": dict(
         bins=(3, 1, 4),
