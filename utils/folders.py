563d90674b384855d2c2484759258a406963cbe6
Piero Viscone <piero.viscone@cern.ch>
2025-02-04 20:52:20 +0100

    new folder definition

--- utils/folders.py
+++ utils/folders.py
@@ -2,25 +2,21 @@ class Folders:
     def __init__(self):
         self.cache = "zzcache"
         self.log = "zlog"
+
         self.snap_path = "zsnap/era{era}/{flow}"
         self.snap = self.snap_path+"/{name}.root" #! MERGE ERAS FOR SNAPSHOTS MAKE NO SENSE
 
-        self.plots = ""
-        self.cards_path = ""
-        self.cards = ""
+        self.cards_path = "zcards"
+        self.cards = self.cards_path+"/{name}_{era}_{flow}.txt"
+
         self.tables_path = ""
         self.tables = ""
 
     def init(self, mergeEras=False, mergeErasYields=False):
         if not mergeEras:
-            self.plots = "era{era}/{flow}"
-            self.cards_path = "zcards/era{era}/{flow}"
-
+            self.plots_path = "era{era}/{flow}"
         else:
-            self.plots = "{flow}"
-            self.cards_path = "zcards/{flow}"
-
-        self.cards = self.cards_path+"/{name}.txt"
+            self.plots_path = "{flow}"
 
         if not mergeErasYields:
             self.tables_path = "ztables/era{era}/{flow}"
