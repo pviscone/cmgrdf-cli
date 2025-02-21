88f888ff4e93a0c19b72121a6851abfafcc1365e
Piero Viscone <piero.viscone@cern.ch>
2025-02-21 18:10:01 +0100

    fixed issue with missing density attribute in plot_utils

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -21,7 +21,7 @@ def __drawPyPlots(path, all_processes, plot, plot_lumi, cmstext, lumitext, noSta
         h = TH1(cmstext = cmstext,
             lumitext= lumitext,
             xlabel = plot.xlabel if not ratio else None,
-            ylabel = "Density" if plot.density else "Events",
+            ylabel = "Density" if getattr(plot, "density", False) else "Events",
             log = plot.log,
             fig = fig,
             ax = ax[0],
@@ -31,7 +31,7 @@ def __drawPyPlots(path, all_processes, plot, plot_lumi, cmstext, lumitext, noSta
         data_hist = file.get("data", False)
         if data_hist:
             data_hist = data_hist.to_hist()
-            h.add(data_hist, label="Data", density =plot.density, color = "black", histtype = "errorbar", w2method="poisson")
+            h.add(data_hist, label="Data", density = getattr(plot, "density", False), color = "black", histtype = "errorbar", w2method="poisson")
 
         if noStack or len(bkgs) == 0:
             for process_name, process_dict in all_processes.items():
@@ -39,7 +39,7 @@ def __drawPyPlots(path, all_processes, plot, plot_lumi, cmstext, lumitext, noSta
                     continue
                 hist = file[process_name].to_hist()
                 color = process_dict.get("color", None)
-                h.add(hist, label=process_dict["label"], density = plot.density, color = color, w2method="poisson")
+                h.add(hist, label=process_dict["label"], density = getattr(plot, "density", False), color = color, w2method="poisson")
         else: #stack
             bkg_hist = [file[bkg].to_hist() for bkg in bkgs if bkg in file]
             bkg_labels = [all_processes[bkg]["label"] for bkg in bkgs if bkg in file]
@@ -52,11 +52,11 @@ def __drawPyPlots(path, all_processes, plot, plot_lumi, cmstext, lumitext, noSta
             stack_total = sum(bkg_hist)
             if stackSignal:
                 stack_total = stack_total + sum(signal_hist)
-            h.add(bkg_hist, label=bkg_labels, density = plot.density, color = bkg_colors, stack = True, histtype = "fill")
+            h.add(bkg_hist, label=bkg_labels, density = getattr(plot, "density", False), color = bkg_colors, stack = True, histtype = "fill")
             signal_histtype = "step" if not stackSignal else "fill"
-            h.add(signal_hist, label=signal_labels, density = plot.density, color = signal_colors, stack = stackSignal, histtype = signal_histtype, w2method="poisson")
-            h.add(stack_total, density = plot.density, color = "black", histtype = "step", yerr=False, linewidth=1)
-            h.add(stack_total, density = plot.density, color = "black", histtype = "band", label="Total Unc.", w2method="poisson")
+            h.add(signal_hist, label=signal_labels, density = getattr(plot, "density", False), color = signal_colors, stack = stackSignal, histtype = signal_histtype, w2method="poisson")
+            h.add(stack_total, density = getattr(plot, "density", False), color = "black", histtype = "step", yerr=False, linewidth=1)
+            h.add(stack_total, density = getattr(plot, "density", False), color = "black", histtype = "band", label="Total Unc.", w2method="poisson")
 
             if ratio:
                 plt.setp(ax[0].get_yticklabels()[0], visible=False)
@@ -90,7 +90,7 @@ def __drawPyPlots(path, all_processes, plot, plot_lumi, cmstext, lumitext, noSta
                     log = plot.log,
                     grid=grid)
             hist = file[process_name].to_hist()
-            h.add(hist, density = plot.density)
+            h.add(hist, density = getattr(plot, "density", False))
             folder = path.replace('.root','')
             folder = folder.rsplit('/',1)[0]+"/2D_"+folder.rsplit('/',1)[1]
             os.makedirs(folder, exist_ok=True)
