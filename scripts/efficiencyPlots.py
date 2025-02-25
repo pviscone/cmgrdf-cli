65e9fbf74d3140cc7eab402d0a22049100bf50e3
Piero Viscone <piero.viscone@cern.ch>
2025-02-25 23:34:30 +0100

    Added allEras option to efficiencyPlots.py

--- scripts/efficiencyPlots.py
+++ scripts/efficiencyPlots.py
@@ -58,62 +58,69 @@ _ncpu = os.cpu_count()
 @app.command()
 def plot_efficiency(
     #! ---------------------- Configs ---------------------- #
-    inputfolder  : Annotated[str , typer.Option("-i", "--in", help="Path of the cmgrdf outfolder (containing all the flow_name/root_files)", rich_help_panel="Configs")],
-    cfgfile      : Annotated[str , typer.Option("-c", "--cfg", help="Path of the yaml config file", rich_help_panel="Configs")],
-    outfolder    : str = typer.Option("zeff", "-o", "--out", help="Output folder", rich_help_panel="Configs"),
-    allvars      : bool = typer.Option(False, "-a", "--all", help="Plot all variables", rich_help_panel="Configs"),
-    ncpu         : int = typer.Option(_ncpu, "-j", "--ncpu", help="Number of cpus", rich_help_panel="Configs"),
-    noReplace   : bool = typer.Option(False, "-n", "--noReplace", help="Do not replace existing plots", rich_help_panel="Configs"),
+    inputfolder    : Annotated[str , typer.Option("-i", "--in", help="Path of the cmgrdf outfolder (containing all the flow_name/root_files)", rich_help_panel="Configs")],
+    cfgfile        : Annotated[str , typer.Option("-c", "--cfg", help="Path of the yaml config file", rich_help_panel="Configs")],
+    outfolder      : str  = typer.Option("zeff", "-o", "--out", help="Output folder", rich_help_panel="Configs"),
+    allvars        : bool = typer.Option(False, "-a", "--all", help="Plot all variables", rich_help_panel="Configs"),
+    ncpu           : int  = typer.Option(_ncpu, "-j", "--ncpu", help="Number of cpus", rich_help_panel="Configs"),
+    noReplace      : bool = typer.Option(False, "-n", "--noReplace", help="Do not replace existing plots", rich_help_panel="Configs"),
+    allEras        : bool = typer.Option(False, "-e", "--allEras", help="Plot all eras", rich_help_panel="Configs"),
 
     #! ---------------------- Plot arguments ---------------------- #
-    rebin        : int  = typer.Option(1, "-r", "--rebin", help="rebin factor", rich_help_panel="Plot"),
-    ylim         : Tuple[float, float] = typer.Option([0,1.2], "-y", "--ylim", help="Y axis limits", rich_help_panel="Plot"),
-    cmsloc       : int = typer.Option(1, "--cmsloc", help="CMS location", rich_help_panel="Plot"),
-    cmstext      : str = typer.Option("Preliminary", "--cmstext", help="CMS text", rich_help_panel="Plot"),
-    grid         : bool = typer.Option(False, "--grid", help="Grid", rich_help_panel="Plot"),
+    rebin          : int  = typer.Option(1, "-r", "--rebin", help="rebin factor", rich_help_panel="Plot"),
+    ylim           : Tuple[float, float] = typer.Option([0,1.2], "-y", "--ylim", help="Y axis limits", rich_help_panel="Plot"),
+    cmsloc         : int  = typer.Option(1, "--cmsloc", help="CMS location", rich_help_panel="Plot"),
+    cmstext        : str  = typer.Option("Preliminary", "--cmstext", help="CMS text", rich_help_panel="Plot"),
+    grid           : bool = typer.Option(False, "--grid", help="Grid", rich_help_panel="Plot"),
 
 
     #! ---------------------- Legend arguments ---------------------- #
-    bbox_to_anchor: Tuple[float, float] = typer.Option((0.5, -0.25), "-b", "--bbox", help="bbox_to_anchor", rich_help_panel="Legend"),
-    loc           : str = typer.Option("lower center", "-l", "--loc", help="loc", rich_help_panel="Legend"),
-    ncol          : int = typer.Option(3, "--ncol", help="ncol", rich_help_panel="Legend"),
-    fontsize      : float = typer.Option(15, "-f", "--fontsize", help="fontsize", rich_help_panel="Legend"),
+    bbox_to_anchor : Tuple[float, float] = typer.Option((0.5, -0.25), "-b", "--bbox", help="bbox_to_anchor", rich_help_panel="Legend"),
+    loc            : str   = typer.Option("lower center", "-l", "--loc", help="loc", rich_help_panel="Legend"),
+    ncol           : int   = typer.Option(3, "--ncol", help="ncol", rich_help_panel="Legend"),
+    fontsize       : float = typer.Option(15, "-f", "--fontsize", help="fontsize", rich_help_panel="Legend"),
 ):
-    inputfolder = os.path.abspath(inputfolder)
-    yaml_cfg = yaml.safe_load(open(cfgfile))
+    if allEras:
+        inputfolders = glob.glob(os.path.join(os.path.abspath(inputfolder), "era*"))
+    else:
+        inputfolders = [os.path.abspath(inputfolder)]
 
-    denom_nums = yaml_cfg["denom_nums"]
-    variables = yaml_cfg.get("vars", None) if not allvars else None
+    for inputfolder in inputfolders:
+        print(f"Processing {inputfolder}")
+        yaml_cfg = yaml.safe_load(open(cfgfile))
 
-    legend_kwargs = dict(bbox_to_anchor=bbox_to_anchor, loc=loc, ncol=ncol, fontsize=fontsize)
-    teff_kwargs = dict(rebin=rebin, ylim=ylim, legend_kwargs=legend_kwargs, cmsloc=cmsloc, cmstext=cmstext, grid=grid)
+        denom_nums = yaml_cfg["denom_nums"]
+        variables = yaml_cfg.get("vars", None) if not allvars else None
 
-    pool_data=[]
-    os.makedirs(os.path.join(inputfolder, outfolder), exist_ok=True)
+        legend_kwargs = dict(bbox_to_anchor=bbox_to_anchor, loc=loc, ncol=ncol, fontsize=fontsize)
+        teff_kwargs = dict(rebin=rebin, ylim=ylim, legend_kwargs=legend_kwargs, cmsloc=cmsloc, cmstext=cmstext, grid=grid)
 
-    for denom, eff_dict in denom_nums.items():
-        for effplot_name, nums_dict in eff_dict.items():
-            if variables is None:
-                variables = glob.glob(os.path.join(inputfolder, f'{denom}/*.root'))
-                variables = [v.split("/")[-1].split(".root")[0] for v in variables]
-            for variable in variables:
-                pool_data.append((inputfolder, outfolder, denom, effplot_name, nums_dict, variable, teff_kwargs, noReplace))
+        pool_data=[]
+        os.makedirs(os.path.join(inputfolder, outfolder), exist_ok=True)
 
-    with concurrent.futures.ProcessPoolExecutor(ncpu) as executor:
-        chunksize = len(pool_data)//ncpu if len(pool_data)//ncpu > 0 else 1
-        list(executor.map(eff_plot, pool_data, chunksize = chunksize))
-    #for data in pool_data:
-    #    eff_plot(*data)
+        for denom, eff_dict in denom_nums.items():
+            for effplot_name, nums_dict in eff_dict.items():
+                if variables is None:
+                    variables = glob.glob(os.path.join(inputfolder, f'{denom}/*.root'))
+                    variables = [v.split("/")[-1].split(".root")[0] for v in variables]
+                for variable in variables:
+                    pool_data.append((inputfolder, outfolder, denom, effplot_name, nums_dict, variable, teff_kwargs, noReplace))
 
-    os.makedirs(os.path.join(inputfolder, outfolder, "scripts"), exist_ok=True)
-    os.makedirs(os.path.join(inputfolder, outfolder, "scripts", "eff_cfg"), exist_ok=True)
-    os.system(f"cp {os.environ['ANALYSIS_DIR']}/scripts/efficiencyPlots.py {os.path.join(inputfolder, outfolder, 'scripts')}")
-    os.system(f"cp {os.path.abspath(cfgfile)} {os.path.join(inputfolder, outfolder, 'scripts', 'eff_cfg')}")
+        with concurrent.futures.ProcessPoolExecutor(ncpu) as executor:
+            chunksize = len(pool_data)//ncpu if len(pool_data)//ncpu > 0 else 1
+            list(executor.map(eff_plot, pool_data, chunksize = chunksize))
+        #for data in pool_data:
+        #    eff_plot(*data)
 
-    command = " ".join(sys.argv).replace('"', r'\\\"')
-    os.system(fr'echo "python {command}" > {os.path.join(inputfolder, outfolder, "command.sh")}')
-    os.system(f"chmod +x {os.path.join(inputfolder, outfolder, 'command.sh')}")
-    copy_file_to_subdirectories(os.path.join(os.environ["CMGRDF"], "externals/index.php"), os.path.join(inputfolder, outfolder))
+        os.makedirs(os.path.join(inputfolder, outfolder, "scripts"), exist_ok=True)
+        os.makedirs(os.path.join(inputfolder, outfolder, "scripts", "eff_cfg"), exist_ok=True)
+        os.system(f"cp {os.environ['ANALYSIS_DIR']}/scripts/efficiencyPlots.py {os.path.join(inputfolder, outfolder, 'scripts')}")
+        os.system(f"cp {os.path.abspath(cfgfile)} {os.path.join(inputfolder, outfolder, 'scripts', 'eff_cfg')}")
+
+        command = " ".join(sys.argv).replace('"', r'\\\"')
+        os.system(fr'echo "python {command}" > {os.path.join(inputfolder, outfolder, "command.sh")}')
+        os.system(f"chmod +x {os.path.join(inputfolder, outfolder, 'command.sh')}")
+        copy_file_to_subdirectories(os.path.join(os.environ["CMGRDF"], "externals/index.php"), os.path.join(inputfolder, outfolder))
 
 if __name__ == "__main__":
     app()
