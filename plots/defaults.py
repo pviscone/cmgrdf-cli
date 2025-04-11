04dc0c8eba51d1c3452640a9e4adb92edbc79612
Piero Viscone <piero.viscone@cern.ch>
2025-04-11 04:14:29 +0200

    adjusted binning

--- plots/defaults.py
+++ plots/defaults.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+import numpy as np
 
 # Priority: global_defaults < histo1d_defaults < branch_defaults < user defined
 global_defaults = OrderedDict()
@@ -10,10 +11,12 @@ histo1d_defaults = OrderedDict(
 
 histo2d_defaults = OrderedDict()
 
+histo3d_defaults = OrderedDict()
+
 # The first matched pattern will be used
 name_defaults = OrderedDict({
     "(.*)_(?:pt|invPt)(.*)": dict(
-        bins=(50, 0, 50),
+        bins=(40, 0, 80),
         label="($1) ($2) $p_{T}$ [GeV]",
     ),
     "(.*)_deta(.*)": dict(
@@ -21,24 +24,25 @@ name_defaults = OrderedDict({
         label="($1) ($2) $|\Delta \eta|$",
     ),
     "(.*)_eta(.*)": dict(
-        bins=(30, -4, 4),
+        bins=(20, -4, 4),
         label="($1) ($2) $\eta$",
     ),
     "(.*)_abseta(.*)": dict(
-        bins=(30, 0, 4),
+        bins=(20, 0, 4),
         label="($1) ($2) $|\eta|$",
     ),
     "(.*)_phi(.*)": dict(
-        bins=(30, -3.14, 3.14),
+        bins=(20, -3.14, 3.14),
         label="($1) ($2) $\phi$",
     ),
     "(.*)_(mass|invMass)(.*)": dict(
-        bins=(40, 0, 6),
+        bins=(30, 0, 15),
         label="($1) ($2) m [GeV]",
     ),
     "(.*)_dR(.*)": dict(
-        bins=(40, 0, 1.5),
+        bins=list(np.logspace(-2, np.log(5), 30)),
         label="($1) ($2) $\Delta$R",
+        log="axis",
     ),
     "(.*)_type(.*)": dict(
         bins=(3, 1, 4),
