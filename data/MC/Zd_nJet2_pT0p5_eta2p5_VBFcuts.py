397ad1865d244dfe4a3834d9cc2e3ba02ddb2c18
Piero Viscone <piero.viscone@cern.ch>
2025-04-11 04:14:13 +0200

    added new VBF mass points

--- data/MC/Zd_nJet2_pT0p5_eta2p5_VBFcuts.py
+++ data/MC/Zd_nJet2_pT0p5_eta2p5_VBFcuts.py
@@ -1,6 +1,62 @@
 from data.MC import cms10
 
 MCDict={
+    "HAHM_13p6TeV_M10_VBF" : {
+        "groups" : [
+            {
+            "name": "HAHM_13p6TeV_M10_VBF",
+            "samples": {
+                "HAHM_13p6TeV_M10_VBF":
+                    {
+                    "xsec": 0.002831,
+                    "path": "signalSamples/HAHM_DarkPhoton_13p6TeV_Nov2024/NANOAOD_noselection_addGEN_VBFflags/CRAB_UserFiles/crab_{name}/250312_175531/0000/DoubleElectronNANO_Run3_{era}_mc_2025Mar12_1.root"
+                    },
+                },
+            "cut":"1",
+            },
+        ],
+        "signal": True,
+        "label":"ZdM10",
+        "color": cms10[0],
+    },
+    "HAHM_13p6TeV_M8_VBF" : {
+        "groups" : [
+            {
+            "name": "HAHM_13p6TeV_M8_VBF",
+            "samples": {
+                "HAHM_13p6TeV_M8_VBF":
+                    {
+                    "xsec": 0.003296,
+                    "path": "signalSamples/HAHM_DarkPhoton_13p6TeV_Nov2024/NANOAOD_noselection_addGEN_VBFflags/CRAB_UserFiles/crab_{name}/250312_175538/0000/DoubleElectronNANO_Run3_{era}_mc_2025Mar12_1.root"
+                    },
+                },
+            "cut":"1",
+            },
+        ],
+        "signal": True,
+        "label":"ZdM8",
+        "color": cms10[1],
+    },
+
+    "HAHM_13p6TeV_M5p5_VBF" : {
+        "groups" : [
+            {
+            "name": "HAHM_13p6TeV_M5p5_VBF",
+            "samples": {
+                "HAHM_13p6TeV_M5p5_VBF":
+                    {
+                    "xsec": 0.003998,
+                    "path": "signalSamples/HAHM_DarkPhoton_13p6TeV_Nov2024/NANOAOD_noselection_addGEN_VBFflags/CRAB_UserFiles/crab_{name}/250312_175545/0000/DoubleElectronNANO_Run3_{era}_mc_2025Mar12_1.root"
+                    },
+                },
+            "cut":"1",
+            },
+        ],
+        "signal": True,
+        "label":"ZdM5p5",
+        "color": cms10[2],
+    },
+
     "HAHM_13p6TeV_M5_VBF" : {
         "groups" : [
             {
@@ -17,7 +73,7 @@ MCDict={
         ],
         "signal": True,
         "label":"ZdM5",
-        "color": cms10[0],
+        "color": cms10[3],
     },
     "HAHM_13p6TeV_M3p1_VBF" : {
         "groups" : [
@@ -34,7 +90,7 @@ MCDict={
             },
         ],
         "label":"ZdM3p1",
-        "color": cms10[1],
+        "color": cms10[4],
         "signal": True,
     },
     "HAHM_13p6TeV_M1_VBF" : {
@@ -53,7 +109,7 @@ MCDict={
         ],
         "label":"ZdM1",
         "signal": True,
-        "color": cms10[2],
+        "color": cms10[5],
     }
 }
 
