268ef1473c16965b925d4674259208c860779019
Piero Viscone <piero.viscone@cern.ch>
2025-04-15 22:41:22 +0200

    Added new mass points

--- data/MC/Zd_nJet012_pTe5_eta1p2.py
+++ data/MC/Zd_nJet012_pTe5_eta1p2.py
@@ -1,22 +1,23 @@
 from data.MC import cms10
 
 MCDict={
-    "HAHM_13p6TeV_M5" : {
+
+    "HAHM_13p6TeV_M1" : {
         "groups" : [
             {
-            "name": "HAHM_13p6TeV_M5",
+            "name": "HAHM_13p6TeV_M1",
             "samples": {
-                "HAHM_13p6TeV_M5":
+                "HAHM_13p6TeV_M1":
                     {
-                    "xsec": 1.952,
-                    "path": "signalSamples/HAHM_DarkPhoton_13p6TeV_Nov2024/NANOAOD_noskim_allpairs_allnanoColl/CRAB_UserFiles/crab_{name}/250124_183927/0000/DoubleElectronNANO_Run3_{era}_mc_2025Jan24_1.root"
+                    "xsec":3.396,
+                    "path": "signalSamples/HAHM_DarkPhoton_13p6TeV_Nov2024/NANOAOD_noselection_addGEN_VBFflags/CRAB_UserFiles/crab_{name}_Inclusive/250207_160036/0000/DoubleElectronNANO_Run3_{era}_mc_2025Feb07_1.root"
                     },
                 },
             "cut":"1",
             },
         ],
+        "label":"ZdM1",
         "signal": True,
-        "label":"ZdM5",
         "color": cms10[0],
     },
     "HAHM_13p6TeV_M3p1" : {
@@ -27,7 +28,7 @@ MCDict={
                 "HAHM_13p6TeV_M3p1":
                     {
                     "xsec":2.679,
-                    "path": "signalSamples/HAHM_DarkPhoton_13p6TeV_Nov2024/NANOAOD_noskim_allpairs_allnanoColl/CRAB_UserFiles/crab_{name}/250124_203010/0000/DoubleElectronNANO_Run3_{era}_mc_2025Jan24_1.root"
+                    "path": "signalSamples/HAHM_DarkPhoton_13p6TeV_Nov2024/NANOAOD_noselection_addGEN_VBFflags/CRAB_UserFiles/crab_{name}_Inclusive/250207_160029/0000/DoubleElectronNANO_Run3_{era}_mc_2025Feb07_1.root"
                     },
                 },
             "cut":"1",
@@ -37,23 +38,77 @@ MCDict={
         "color": cms10[1],
         "signal": True,
     },
-    "HAHM_13p6TeV_M1" : {
+    "HAHM_13p6TeV_M5" : {
         "groups" : [
             {
-            "name": "HAHM_13p6TeV_M1",
+            "name": "HAHM_13p6TeV_M5",
             "samples": {
-                "HAHM_13p6TeV_M1":
+                "HAHM_13p6TeV_M5":
                     {
-                    "xsec":3.396,
-                    "path": "signalSamples/HAHM_DarkPhoton_13p6TeV_Nov2024/NANOAOD_noskim_allpairs_allnanoColl/CRAB_UserFiles/crab_{name}/250124_183942/0000/DoubleElectronNANO_Run3_{era}_mc_2025Jan24_1.root"
+                    "xsec": 1.952,
+                    "path": "signalSamples/HAHM_DarkPhoton_13p6TeV_Nov2024/NANOAOD_noselection_addGEN_VBFflags/CRAB_UserFiles/crab_{name}_Inclusive/250207_160022/0000/DoubleElectronNANO_Run3_{era}_mc_2025Feb07_1.root"
                     },
                 },
             "cut":"1",
             },
         ],
-        "label":"ZdM1",
         "signal": True,
+        "label":"ZdM5",
         "color": cms10[2],
-    }
+    },
+    "HAHM_13p6TeV_M5p5" : {
+        "groups" : [
+            {
+            "name": "HAHM_13p6TeV_M5p5",
+            "samples": {
+                "HAHM_13p6TeV_M5p5":
+                    {
+                    "xsec": 1.910,
+                    "path": "signalSamples/HAHM_DarkPhoton_13p6TeV_Nov2024/NANOAOD_noselection_addGEN_VBFflags/CRAB_UserFiles/crab_{name}_Inclusive/250312_175524/0000/DoubleElectronNANO_Run3_{era}_mc_2025Mar12_1.root"
+                    },
+                },
+            "cut":"1",
+            },
+        ],
+        "signal": True,
+        "label":"ZdM5p5",
+        "color": cms10[3],
+    },
+    "HAHM_13p6TeV_M6" : {
+        "groups" : [
+            {
+            "name": "HAHM_13p6TeV_M6",
+            "samples": {
+                "HAHM_13p6TeV_M6":
+                    {
+                    "xsec": 1.865,
+                    "path": "signalSamples/HAHM_DarkPhoton_13p6TeV_Nov2024/NANOAOD_noselection_addGEN_VBFflags/CRAB_UserFiles/crab_{name}_Inclusive/250312_175516/0000/DoubleElectronNANO_Run3_{era}_mc_2025Mar12_1.root"
+                    },
+                },
+            "cut":"1",
+            },
+        ],
+        "signal": True,
+        "label":"ZdM6",
+        "color": cms10[4],
+    },
+    "HAHM_13p6TeV_M6p5" : {
+        "groups" : [
+            {
+            "name": "HAHM_13p6TeV_M6p5",
+            "samples": {
+                "HAHM_13p6TeV_M6p5":
+                    {
+                    "xsec": 1.834,
+                    "path": "signalSamples/HAHM_DarkPhoton_13p6TeV_Nov2024/NANOAOD_noselection_addGEN_VBFflags/CRAB_UserFiles/crab_{name}_Inclusive/250312_175509/0000/DoubleElectronNANO_Run3_{era}_mc_2025Mar12_1.root"
+                    },
+                },
+            "cut":"1",
+            },
+        ],
+        "signal": True,
+        "label":"ZdM6p5",
+        "color": cms10[5],
+    },
 }
 
