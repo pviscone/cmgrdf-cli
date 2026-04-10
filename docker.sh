865b534ccceba462e43b3bf3c9cf54756e4cc2e7
Piero Viscone <pieroviscone@gmail.com>
2026-04-10 17:48:34 +0200

    Added docker setup

---
+++ docker.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# 1. Handle Container argument (usage: ./script.sh custom-image:tag)
+CONTAINER=${1:-"el10-root-master:latest"}
+shift
+
+USER_NAME=$(whoami)
+USER_INITIAL=${USER_NAME:0:1}
+DOCKER_FLAGS=("-it" "--rm" "--network" "host")
+
+# 2. Detect the SSH key
+if [ -f "$HOME/.ssh/id_ed25519" ]; then
+    SSH_KEY="~/.ssh/id_ed25519"
+elif [ -f "$HOME/.ssh/id_rsa" ]; then
+    SSH_KEY="~/.ssh/id_rsa"
+fi
+
+# 3. Base Mounts and Environment
+DOCKER_FLAGS+=("-v" "$HOME:$HOME")
+DOCKER_FLAGS+=("-e" "HOME=$HOME")
+DOCKER_FLAGS+=("-e" "GIT_SSH_COMMAND=ssh -i $SSH_KEY -o IdentitiesOnly=yes -o StrictHostKeyChecking=no")
+DOCKER_FLAGS+=("-v" "/etc/passwd:/etc/passwd:ro")
+DOCKER_FLAGS+=("-v" "/etc/group:/etc/group:ro")
+DOCKER_FLAGS+=("-u" "$(id -u):$(id -g)")
+DOCKER_FLAGS+=("-v" "$(pwd):$(pwd)")  # Mount current host dir to same path in container
+DOCKER_FLAGS+=("-w" "$(pwd)")         # Set the starting directory inside the container
+DOCKER_FLAGS+=("-e" "PYTHONNOUSERSITE=1")
+DOCKER_FLAGS+=("-e" "PYTHONPATH=/usr/local/root_install/lib:/usr/local/lib64/python3.12/site-packages:/usr/local/lib/python3.12/site-packages")
+DOCKER_FLAGS+=("-e" "LD_PRELOAD=")
+
+# 4. Conditional Mounts (CVMFS, AFS, EOS)
+mount_if_exists() {
+    if [ -d "$1" ]; then
+        echo "Mounting $1..."
+        DOCKER_FLAGS+=("-v" "$1:$1")
+    else
+        echo "Skipping $1 (Not found on host)"
+    fi
+}
+
+# Check CVMFS
+mount_if_exists "/cvmfs"
+
+# Check AFS Work
+mount_if_exists "/afs/cern.ch/work/$USER_INITIAL/$USER_NAME"
+
+# Check EOS User
+mount_if_exists "/eos/user/$USER_INITIAL/$USER_NAME"
+
+# Check EOS CMS Store
+mount_if_exists "/eos/cms/store/cmst3"
+
+# 5. Run it
+echo "Starting container: $CONTAINER"
+docker run "${DOCKER_FLAGS[@]}" "$CONTAINER" "$@"
---
+++ setup_docker.sh
@@ -0,0 +1,35 @@
+# Check if the script is being sourced in bash, otherwise raise an error
+export CMGRDF_CLI=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+
+CURRENT_SHELL=$(ps -p $$ -o comm=)
+if [[ "$CURRENT_SHELL" == "zsh" ]]; then
+    echo "ZSH can't source the cvmfs scripts. You must source it in bash and then return to zsh" >&2
+    return 1
+fi
+
+# Check if the script is being sourced in bash, otherwise raise an error
+if [[ ${BASH_SOURCE[0]} == $0 ]]; then
+   echo "You must source this script, not execute it. Run 'source setup.sh'" >&2
+   exit 1
+fi
+
+CURRENT_PWD=$(pwd)
+cd $CMGRDF_CLI/cmgrdf-prototype
+# Check if the user wants to build cmgrdf
+if [[ "$1" == "build" ]]; then
+    make clean
+    make -j 8
+fi
+
+# Set the environment variables
+eval $(make env)
+if [[ "$1" == "build" ]]; then
+    python3 -m venv "$CMGRDF_CLI/.venv"
+    source "$CMGRDF_CLI/.venv/bin/activate"
+    pip install -r $CMGRDF_CLI/requirements.txt
+else
+    source "$CMGRDF_CLI/.venv/bin/activate"
+fi
+
+export PATH=$CMGRDF_CLI/.venv/bin:$CMGRDF_CLI:$CMGRDF_CLI/scripts:$CMGRDF_CLI/bin:$PATH
+cd $CURRENT_PWD
