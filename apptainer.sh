6d05b8a867af49ea94c5df800f37684981315e29
Piero Viscone <piero.viscone@cern.ch>
2026-04-11 04:16:04 +0200

    Added apptainer runner script

---
+++ apptainer.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# 1. Setup local tmp/cache to avoid /tmp permission errors
+export BASEAPPTAINER=${APPTAINER:-"$(pwd)/.apptainer"}
+
+export APPTAINER_TMPDIR="/tmp/$(whoami)_apptainer_tmp"
+export APPTAINER_CACHEDIR="$BASEAPPTAINER/cache"
+mkdir -p "$APPTAINER_TMPDIR" "$APPTAINER_CACHEDIR"
+
+# 2. Container Image
+CONTAINER_URI=${1:-"docker://pviscone/cmgrdf-cli:el10-root-master"}
+shift
+
+# 3. Detect SSH Key
+SSH_KEY="$HOME/.ssh/id_rsa"
+[ -f "$HOME/.ssh/id_ed25519" ] && SSH_KEY="$HOME/.ssh/id_ed25519"
+
+# 4. Apptainer Flags (The "Host" Setup)
+# We REMOVE --net and --network. Apptainer will now use host network by default.
+# We keep --userns to ensure it works even if the system SUID is wonky.
+APPTAINER_FLAGS=("--userns")
+
+# 5. Environment Variables (Internal to Container)
+export APPTAINERENV_GIT_SSH_COMMAND="ssh -i $SSH_KEY -o IdentitiesOnly=yes -o StrictHostKeyChecking=no"
+export APPTAINERENV_PYTHONNOUSERSITE=1
+export APPTAINERENV_PYTHONPATH="/usr/local/root_install/lib:/usr/local/lib64/python3.12/site-packages:/usr/local/lib/python3.12/site-packages"
+
+# 6. Bind Mounts
+bind_if_exists() {
+    [ -d "$1" ] && APPTAINER_FLAGS+=("-B" "$1:$1")
+}
+
+# Standard CERN/CMS mounts
+bind_if_exists "/cvmfs"
+bind_if_exists "/eos"
+bind_if_exists "/afs"
+bind_if_exists "/etc/grid-security"
+bind_if_exists "/etc/vomses"
+
+# 7. Run
+echo "Running in HOST network mode..."
+apptainer run "${APPTAINER_FLAGS[@]}" "$CONTAINER_URI" "$@"
